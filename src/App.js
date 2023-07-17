2833d8bc6b5f0f531cc027bfbe74445a0fa6d547
21stChrom <81393334+21stChrom@users.noreply.github.com>
2023-07-17 01:20:18 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <span className="counter__output">{counter}</span>
         <div className="btn__container">
           <button className="control__btn" onClick={decrease}>-</button>
-          <button className="reset" onClick={reset}>Reset</button>
+          <button     className="reset" onClick={reset}>Reset</button>
           <button className="control__btn" onClick={increase}>+</button>
         </div>
       </div>
